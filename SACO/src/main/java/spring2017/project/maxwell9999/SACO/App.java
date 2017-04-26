2f6da90745d31ee119eb24a59ad22cd5bbcb844a
Maxwell Taylor <maxwell9999@gmail.com>
2017-04-26 15:02:34 -0700

    maven project structure

---
+++ SACO/src/main/java/spring2017/project/maxwell9999/SACO/App.java
@@ -0,0 +1,13 @@
+package spring2017.project.maxwell9999.SACO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
